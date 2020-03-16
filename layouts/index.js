ae68255ac9945e37a53dab1b95f10cf0958a4ce7
alonavns <alona.vns@outlook.com>
2020-03-16 16:43:49 +0200

    logo

--- layouts/index.js
+++ layouts/index.js
@@ -9,6 +9,13 @@ const Account = styled.p`
   margin: 0;
   padding: 10px;
   text-align: center;
+
+  .logo {
+    position: absolute;
+    left: 10px;
+    height: 34px;
+    top: 5px;
+  }
 `
 
 const Content = styled.div`
@@ -21,6 +28,7 @@ export default connect(state => state)(function({ children, ...props }) {
   return (
     <Wrapper>
       <Account>
+        <img src="https://picsum.photos/30" className="logo" />
         {address || '---'} : {balance || '0'}
       </Account>
       <Content>{children}</Content>
