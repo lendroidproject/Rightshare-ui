f3d3822fd4c16b2ccb9d9e4a7c8011526ebfc384
alonavns <alona.vns@outlook.com>
2020-03-23 07:22:20 +0200

    next-offline

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
+const withOffline = require('next-offline')
 const withImages = require('next-images')
 
-module.exports = withImages({
-  env: {
-    // Reference a variable that was defined in the .env file and make it available at Build Time
-    OPENSEA_API_URL: process.env.OPENSEA_API_URL,
-    OPENSEA_API_KEY: process.env.OPENSEA_API_KEY,
-  },
-})
+module.exports = withOffline(
+  withImages({
+    env: {
+      // Reference a variable that was defined in the .env file and make it available at Build Time
+      OPENSEA_API_URL: process.env.OPENSEA_API_URL,
+      OPENSEA_API_KEY: process.env.OPENSEA_API_KEY,
+    },
+  })
+)
