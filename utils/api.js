f459177c72a29447c0455bbe5e86895c8ce83981
Vignesh Meenakshi Sundaram <vignesh.msundaram@gmail.com>
2020-03-29 18:21:16 +0530

    Fix false positive of CORS during axios requests

--- utils/api.js
+++ utils/api.js
@@ -8,15 +8,14 @@ export function getBaseURL() {
   return apiEndpoint
 }
 
-// axios.defaults.withCredentials = true
+axios.defaults.withCredentials = true
 axios.defaults.baseURL = getBaseURL()
 axios.defaults.timeout = 30 * 1000 // Max time limit: 30s
 axios.defaults.method = 'GET'
 axios.defaults.headers = {
-  Accept: 'application/json',
+  'Accept': 'application/json',
   'Content-Type': 'application/json',
-  'X-API-KEY': OPENSEA_API_KEY,
-  'Access-Control-Allow-Origin': '*',
+  'x-api-key': OPENSEA_API_KEY
 }
 
 function jsonConfig(config) {
@@ -36,9 +35,8 @@ export function getMyAssets(params) {
   return request({
     url: '/assets',
     params: {
-      format: 'json',
       ...params,
-    },
+    }
   })
 }
 
