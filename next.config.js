2e97889fa602993c6f137b2ac6175a9cde7375cc
alonavns <alona.vns@outlook.com>
2020-04-09 13:58:30 +0300

    config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = withOffline(
   withImages({
     env: {
       // Reference a variable that was defined in the .env file and make it available at Build Time
-      MAIN_NETWORK: process.env.MAIN_NETWORK,
+      MAIN_NETWORK: process.env.MAIN_NETWORK === 'true',
       OPENSEA_API_KEY: process.env.OPENSEA_API_KEY,
     },
   })
