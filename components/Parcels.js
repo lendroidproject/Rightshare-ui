833dbf9c60f405b44c58bd3846be8546a43641b0
alonavns <alona.vns@outlook.com>
2020-05-26 17:11:44 +0300

    filter other NFTs

--- components/Parcels.js
+++ components/Parcels.js
@@ -6,7 +6,8 @@ export const Addresses = {
   '0x79986af15539de2db9a5086382daeda917a9cf0c': 'CV',
   '0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85': 'ENS',
 }
-const isAddr = (lang, addr) => (Addresses[lang] ? addr.toLowerCase() === Addresses[lang] : true)
+const other = [Addresses.CV, Addresses.ENS]
+const isAddr = (lang, addr) => (Addresses[lang] ? addr.toLowerCase() === Addresses[lang] : !other.includes(addr))
 export const filterPlatform = (lang, getName) => ({ asset_contract: { address } }) =>
   !['FRight', 'IRight'].includes(getName(address)) && isAddr(lang, address)
 export const filterBase = (lang) => ({ base }) => base && isAddr(lang, base[0])
